f9b4223bcf7e8e6dfddf4eac34652a7f562e3573
Renan Veronez Drechsler <renanvz@outlook.com>
2024-01-13 18:59:32 +0100

    React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
-        <Button>Send</Button>
-      </div>
+      <RouterProvider router={router} />
     </>
   )
 }
